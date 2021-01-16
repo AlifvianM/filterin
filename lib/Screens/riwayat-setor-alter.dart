32aaeb6e3d4c9c5128ea6ec0492f4bfa88591393
verkit <verdybangkit@gmail.com>
2021-01-16 14:31:46 +0700

    haha

--- lib/Screens/riwayat-setor-alter.dart
+++ lib/Screens/riwayat-setor-alter.dart
@@ -1,6 +1,7 @@
+import 'package:camera/camera.dart';
+import 'package:filterin/components/cameras.dart';
 import 'package:filterin/helper/setor-controller.dart';
 import 'package:flutter/material.dart';
-import 'package:filterin/components/cameras.dart';
 
 class RiwayatSetor extends StatefulWidget {
   @override
@@ -8,11 +9,10 @@ class RiwayatSetor extends StatefulWidget {
 }
 
 class _RiwayatSetorState extends State<RiwayatSetor> {
-
   List data = [];
 
   void getData() async {
-    SetorC.getData().then((value){
+    SetorC.getData().then((value) {
       setState(() {
         data = value;
       });
@@ -30,42 +30,48 @@ class _RiwayatSetorState extends State<RiwayatSetor> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Riwayat Setor"),
-        backgroundColor:Colors.green,
       ),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Colors.white,
         child: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, i){
-            return Container(
-              margin: EdgeInsets.fromLTRB(10, 15, 10, 0),
-              padding: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                border: Border.all(width: 0.7, color: Colors.black),
-                borderRadius: BorderRadius.all(Radius.circular(15))
-              ),
-              child: Column(
-                children: [
-                  Text('pembayaran : ' + data[i]['pembayaran']),
-                  Text('tgl : ' + data[i]['tanggal']),
-                  Text('jenis sampah : ' + data[i]['jenis_sampah']),
-                  Text('berat : ' + data[i]['berat'].toString()),
-                  RaisedButton(
-                    child: Text("Tambah Bukti Pembayaran"),
-                    onPressed: () {
-                      Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => TakePictureScreen()),
-            );
-                    }
-                  )
-                ],
-              ),
-            );
-          }
-        ),
+            itemCount: data.length,
+            itemBuilder: (context, i) {
+              return Container(
+                margin: EdgeInsets.fromLTRB(10, 15, 10, 0),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 0.7, color: Colors.black),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                child: Column(
+                  children: [
+                    Text('pembayaran : ' + data[i]['pembayaran']),
+                    Text('tgl : ' + data[i]['tanggal']),
+                    Text('jenis sampah : ' + data[i]['jenis_sampah']),
+                    Text('berat : ' + data[i]['berat'].toString()),
+                    RaisedButton(
+                      onPressed: () async {
+                        // Ensure that plugin services are initialized so that `availableCameras()`
+                        // can be called before `runApp()`
+                        WidgetsFlutterBinding.ensureInitialized();
+
+                        // Obtain a list of the available cameras on the device.
+                        final cameras = await availableCameras();
+
+                        // Get a specific camera from the list of available cameras.
+                        final firstCamera = cameras.first;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => TakePictureScreen(camera: firstCamera)),
+                        );
+                      },
+                      child: Text('a'),
+                    ),
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
